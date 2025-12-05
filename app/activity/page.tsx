e83d42a5e7f2f2980d1a1098a301d1be2e3d0ea1
PresidentTree94 <karlysams1218@gmail.com>
2025-12-05 15:51:22 -0500

    Corrected Vercel vulnerability, nav colors.

--- app/activity/page.tsx
+++ app/activity/page.tsx
@@ -7,6 +7,9 @@ export default function Activity() {
     <>
       <Link href="/" className="text-base flex items-center gap-2 text-emerald-500 font-semibold"><FontAwesomeIcon icon={faArrowLeft} className="!w-auto !h-5" />Back to All Takes</Link>
       <h2 className="text-4xl font-extrabold text-gray-100 flex items-center gap-3"><FontAwesomeIcon icon={faWaveSquare} className="!w-auto !h-8 text-emerald-500" />My Anonymous Activity Tracker</h2>
+      <article className="box p-8">
+        <div className="bg-gray-700 p-4 rounded-lg border border-gray-600"></div>
+      </article>
     </>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,28 +8,33 @@ export default function Navbar() {
 
   const pathname = usePathname();
 
-  let boltClr;
+  let brdrClr, boltClr;
   switch (pathname) {
     case "/activity":
-      boltClr = "emerald";
+      boltClr = "text-emerald-400";
+      brdrClr = "border-emerald-500/20";
       break;
     case "/opinions":
-      boltClr = "indigo";
+      boltClr = "text-indigo-400";
+      brdrClr = "border-indigo-500/20";
       break;
     case "/peeves":
-      boltClr = "rose";
+      boltClr = "text-rose-400";
+      brdrClr = "border-rose-500/20";
       break;
     case "/submit":
-      boltClr = "orange";
+      boltClr = "text-orange-400";
+      brdrClr = "border-orange-500/20";
       break;
     default:
-      boltClr = "yellow";
+      boltClr = "text-yellow-400";
+      brdrClr = "border-yellow-500/20";
       break;
   }
 
   return (
-    <header className={`flex items-center justify-between bg-gray-800 py-4 px-8 border-b border-${boltClr}-500/20`}>
-      <h1 className="font-extrabold text-3xl text-white flex items-center gap-2"><FontAwesomeIcon icon={faBolt} className={`!w-auto !h-8 text-${boltClr}-400`} />Unpopular Exchange</h1>
+    <header className={`flex items-center justify-between bg-gray-800 py-4 px-8 border-b ${brdrClr}`}>
+      <h1 className="font-extrabold text-3xl text-white flex items-center gap-2"><FontAwesomeIcon icon={faBolt} className={`!w-auto !h-8 ${boltClr}`} />Unpopular Exchange</h1>
       <nav className="flex gap-2">
         <Link href="/activity" className="bg-gray-700 py-1.5 px-3 rounded-full flex items-center gap-2"><FontAwesomeIcon icon={faWaveSquare} className="!w-auto !h-4" />My Activity</Link>
         <Link href="/" className="bg-gray-700 py-1.5 px-3 rounded-full">All</Link>
